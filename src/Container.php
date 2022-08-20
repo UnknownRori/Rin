fbd9a58f3b55e9357882d54339fb75947d57ecf3
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-20 07:41:53 +0700

    🔨 Implement Built in Container

---
+++ src/Container.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace UnknownRori\ProjectReiki;
+
+use Psr\Container\ContainerInterface;
+use UnknownRori\ProjectReiki\Exceptions\ContainerKeyNotFound;
+
+/**
+ * Built in Dependency Injection Container, 
+ * not very useful so try use 3rd party library
+ */
+class Container implements ContainerInterface
+{
+    protected $data = [];
+
+    /**
+     * Insert new data into container
+     * @param  string $id
+     * @param  mixed  $data
+     * @return void
+     */
+    public function set(string $id, $data): void
+    {
+        $this->data[$id] = $data;
+    }
+
+    /**
+     * Get the data from container using passed id
+     * @param mixed $name
+     * 
+     * @return mixed
+     */
+    public function get(string $id): mixed
+    {
+        if ($this->has($id))
+            return $this->data[$id];
+
+        throw new ContainerKeyNotFound();
+    }
+
+    /**
+     * Check if the container has a passed id
+     * @param  mixed $id
+     * 
+     * @return bool
+     */
+    public function has(string $id): bool
+    {
+        return array_key_exists($id, $this->data);
+    }
+}
