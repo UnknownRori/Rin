b2b7c0b4aa6882420f780553994c83b573071996
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-20 07:40:02 +0700

    📦 Chore : scaffold the Application Class

---
+++ src/Application.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace UnknownRori\ProjectReiki;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * Application Bootstrap of Project Reiki light weight framework
+ */
+class Application
+{
+    public ?ContainerInterface $container;
+
+    /**
+     *  Initialize Project Reiki Application bootstrap
+     */
+    public function __construct(ContainerInterface $container = new Container())
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Serve the Project Reiki Application
+     */
+    public function serve(): void
+    {
+        //
+    }
+
+    /**
+     * Automatic dependency injection on passed namespace and return the result
+     */
+    protected function inject($namespace): mixed
+    {
+        //
+    }
+}
