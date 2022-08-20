a166b00f33f61c5fc260ab7aece5be63e0b6daf4
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-20 11:55:16 +0700

    📦 chore : scaffold Response class

---
+++ src/Http/Response.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace UnknownRori\ProjectReiki\Http;
+
+use UnknownRori\ProjectReiki\Contracts\Response as IResponse;
+use UnknownRori\ProjectReiki\Contracts\Response\JsonResponse;
+use UnknownRori\ProjectReiki\Contracts\Response\ViewResponse;
+
+class Response implements IResponse, JsonResponse, ViewResponse
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function __destruct()
+    {
+        //
+    }
+
+    /**
+     * Add header on returning response
+     * @param  string $header
+     * @param  string $value
+     * @return self
+     */
+    public function header(string $header, mixed $value): self
+    {
+        return $this;
+    }
+
+    /**
+     * Add header on returning response
+     * @param  string $header
+     * @param  string $value
+     * @return self
+     */
+    public function withHeaders(array $headers): self
+    {
+        return $this;
+    }
+
+    /**
+     * Insert cookie to the response
+     * @param  string $name
+     * @param  mixed  $value
+     * @param  int    $expires
+     * @param  string $path
+     * @param  string $domain
+     * @param  bool   $secure
+     * @param  bool   $httpOnly
+     * @return bool
+     */
+    public function cookie(
+        string $name,
+        mixed $value,
+        int $expires = 60,
+        ?string $path = null,
+        ?string $domain = null,
+        bool $secure = false,
+        bool $httpOnly = false
+    ): bool {
+        return true;
+    }
+
+    /**
+     * Send out view response using the passed filepath that will concatenate 
+     * with view path that defined when initialize the Project Reiki
+     * @param  string  $path
+     * @return self
+     */
+    public function view(array|string $path): self
+    {
+        return $this;
+    }
+
+    /**
+     * Convert passed associative array into json and return it as response
+     * along with passed http code
+     * @param  array $data
+     * @param  int   $httpCode
+     * @return self
+     */
+    public function json(array $data, int $httpCode): self
+    {
+        return $this;
+    }
+}
