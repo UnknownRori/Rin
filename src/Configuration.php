b8e71e3cca325eebb9980e69191d6ed013dafa7e
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-08-22 04:28:53 +0700

    🔨 Make the code readable

--- src/Configuration.php
+++ src/Configuration.php
@@ -5,8 +5,17 @@ namespace UnknownRori\Rin;
 class Configuration
 {
     // View File Location
-    public ?string $viewLocation = './views/';
+    public string $viewLocation = './views/';
+
     // View File Type/Extension
-    public ?string $viewFileType = '.php';
-    public ?array $allowedResource = ['png', 'jpg', 'jpeg', 'gif', 'css', 'js', 'ico', 'svg', 'ttf'];
+    public string $viewFileType = '.php';
+
+    // Session type driver
+    public string $sessionDriver = 'file';
+
+    // Session configuration
+    public array $sessionConfig = [];
+
+    // Allowed resource accessed on public
+    public array $allowedResource = ['png', 'jpg', 'jpeg', 'gif', 'css', 'js', 'ico', 'svg', 'ttf'];
 }
