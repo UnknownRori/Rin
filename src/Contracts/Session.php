5b4f54f8957f01217c3cdfb1e03015133d39a756
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-09-01 13:11:48 +0700

    🔨 added flash on session contract

--- src/Contracts/Session.php
+++ src/Contracts/Session.php
@@ -9,4 +9,6 @@ interface Session
 {
     public function set(string $key, mixed $value);
     public function get(string $key): mixed;
+    public function session(string $key, $value = null): mixed;
+    public function flash(string $key, $value = null): mixed;
 }
--- src/Facades/Session/FileSession.php
+++ src/Facades/Session/FileSession.php
@@ -9,10 +9,14 @@ use UnknownRori\Rin\Contracts\Session;
  */
 class FileSession implements Session
 {
+    protected string $flash = '__flash';
+
     public function __construct()
     {
         if (session_status() == PHP_SESSION_NONE)
             session_start();
+
+        $_SESSION[$this->flash] = [];
     }
 
     public function set(string $key, mixed $value): void
@@ -23,5 +27,21 @@ class FileSession implements Session
     public function get(string $key): mixed
     {
         return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : null;
+    }
+
+    public function session(string $key, $value = null)
+    {
+        if (is_null($value))
+            return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : null;
+
+        $this->set($key, $value);
+    }
+
+    public function flash(string $key, $value = null): mixed
+    {
+        if (is_null($value))
+            return array_key_exists($key, $_SESSION[$this->flash]) ? $_SESSION[$this->flash] : null;
+
+        $_SESSION[$this->flash] = $value;
     }
 }
